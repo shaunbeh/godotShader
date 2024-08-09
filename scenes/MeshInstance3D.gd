bf161b50f79a386eecf8b012feb8e49ea459f2e6
shaunbeh <shahin.behmand@gmail.com>
2024-08-09 22:31:33 +0330

    first shader 3d

---
+++ scenes/MeshInstance3D.gd
@@ -0,0 +1,12 @@
+extends MeshInstance3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	mesh.material.set_shader_parameter("height_scale", 1.0)
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
